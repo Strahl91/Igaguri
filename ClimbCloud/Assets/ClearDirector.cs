32a896f8268af4a60d5bba8b31eb99a72085801b
井上大志 <inouehiroshi@FL1-133-209-242-172.osk.mesh.ad.jp>
2021-03-07 14:54:02 +0900

    猫のクライムアクションゲーム

---
+++ ClimbCloud/Assets/ClearDirector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;//LoadSceneを使う為の宣言
+
+public class ClearDirector : MonoBehaviour
+{
+    public Text ClearTime;
+    public Text BestTime;
+    //float bestTime = 99.0f;
+
+    void Start()
+    {
+        float CurrentTime = PlayerPrefs.GetFloat("CurrentTime");
+
+        ClearTime.text = string.Format("{0:0.00}", CurrentTime);
+
+        if (PlayerPrefs.HasKey("BestTime") == false)
+        {
+            PlayerPrefs.SetFloat("BestTime", CurrentTime+1.0f);
+            PlayerPrefs.Save();
+        }
+
+        float fBestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if (CurrentTime < fBestTime)
+        {
+            PlayerPrefs.SetFloat("BestTime", CurrentTime);
+            PlayerPrefs.Save();
+            fBestTime = CurrentTime;
+        }
+       
+        BestTime.text = string.Format("{0:0.00}", fBestTime);
+
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("GameScene");
+
+        }
+    }
+}
---
+++ ClimbCloud/Assets/GameDirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    public float StartTime = 0;
+    GameObject PlayTime;
+    public GameObject NearGoal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+        this.PlayTime = GameObject.Find("Text");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float ClearTime = GetPlayTime();
+
+        this.PlayTime.GetComponent<Text>().text = string.Format("{0:0.00}", ClearTime);
+    }
+
+    public float GetPlayTime()
+    {
+        return Time.time - StartTime;
+    }
+}
