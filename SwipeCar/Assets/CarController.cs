48da1875915cce4d0ce2c30ba563de3eef725e1f
井上大志 <inouehiroshi@FL1-125-199-17-247.osk.mesh.ad.jp>
2021-03-07 12:36:42 +0900

    車のスワイプゲーム

---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+    float starttime;
+    public float Topspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0)) //マウスがクリックされたら
+        {
+            //マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+            starttime = Time.time;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+            float endtime = Time.time;
+            float deltatime = endtime - starttime;
+
+            //スワイプの長さを初速度に設定する(追加：スワイプの時間に応じて車の移動距離が変化)
+            this.speed = swipeLength / (500 * deltatime);
+            Topspeed = this.speed;
+
+            //効果音再生
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0); //移動
+        this.speed *= 0.98f;                   //減速
+    }
+}
---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;  //GameObject型の変数の宣言
+    Color textcolor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance"); //GameObject型の変数にGameSceneに置いた実際のObjectを結びつける
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+        ColorUtility.TryParseHtmlString("#323232", out textcolor);
+
+        if (length >= -3.0f && length <= 5.0f)
+        {
+            this.distance.GetComponent<Text>().text = "Excellent!!\r\nゴールまで" + length.ToString("F2") + "m";
+            ColorUtility.TryParseHtmlString("#FF0000", out textcolor);
+        }
+        else if (length <= -4.75f)
+        {
+            this.distance.GetComponent<Text>().text = "Game Over!";
+            ColorUtility.TryParseHtmlString("#00008B", out textcolor);
+        }
+        this.distance.GetComponent<Text>().color = textcolor;
+    }
+
+    public void Restart()//リスタートのメソッド。ボタンが押された際に最初のシーンを再び読み込むようにしている。
+    {
+        SceneManager.LoadScene("GameScene");
+
+
+    }
+
+
+
+}
